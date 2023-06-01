3f0aa5b5d47456eb9322496b7341682b7e820293
nareshdocker9447 <nk122121@gmail.com>
2023-06-01 16:27:02 +0100

    vpc

--- VPC/variables.tf
+++ VPC/variables.tf
@@ -1,4 +1,11 @@
-variable "cidr" {
-  type = string
-  default = "10.0.0.0/16"
+# variable "cidr" {
+#   type = string
+#   default = "10.0.0.0/16"
+# # }
+
+variable "tags"{
+  type = map
+  default = {
+  Name = "vpc"
+  }
 }
