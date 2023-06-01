d9b632269dd4009a40c5887cb0a68f6ad540f9f4
nareshdocker9447 <nk122121@gmail.com>
2023-06-01 15:57:13 +0100

    vpc

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,9 +1,6 @@
 # this will create vpc
 resource "aws_vpc" "this" {
-  cidr_block       = var.cidr
+  cidr_block      = var.cidr
   instance_tenancy = "default"
 
-  tags = {
-    Name = "main"
-  }
 }
