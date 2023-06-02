7d8390c15b324cafec0aa1fdf689dd7df97b29f0
nareshdocker9447 <nk122121@gmail.com>
2023-06-02 17:29:11 +0100

    updated

--- VPC/main.tf
+++ VPC/main.tf
@@ -37,3 +37,11 @@ resource "aws_route_table" "public" {
   tags = merge ( var.tags, var.public_route_table_tags, {"Name" = var.public_route_table_name })
 
 }
+
+# aws route table
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
