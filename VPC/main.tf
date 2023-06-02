7b24ab2b47d192fa71a3f8314d40e598e50eb887
nareshdocker9447 <nk122121@gmail.com>
2023-06-02 18:12:08 +0100

    updated

--- VPC/main.tf
+++ VPC/main.tf
@@ -48,6 +48,6 @@ resource "aws_route" "public" {
 
 resource "aws_route_table_association" "public" {
    count = length(var.public_subnet_cidr)
-   subnet_id      = element(aws_subnet.main[*].id, count.index)
+   subnet_id      = element(aws_subnet.public[*].id, count.index)
    route_table_id = aws_route_table.public.id
  }
