1bfdc85ff7f60de25460a58d3d0f59ca226f1e28
nareshdocker9447 <nk122121@gmail.com>
2023-06-05 17:30:02 +0100

    added locals.tf

--- VPC/outputs.tf
+++ VPC/outputs.tf
@@ -6,7 +6,7 @@ output "db_subnet_group_name" {
     value = aws_db_subnet_group.database.name
 }
 
-outpud "private_subnet_ids" {
+output "private_subnet_ids" {
     description = "list of private IDs"
     value = "aws_subnet.private[*].id"
 }
